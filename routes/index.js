4defd6ed50aeb0922f138ec2f0fb9e90e35932b2
kiran-rao248 <kiran.rao@doctoroncall.com.my>
2019-08-15 10:27:27 +0800

    header & footer added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const lib = require('../config/library'); 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {  
+  res.render('index', { 
+    title: 'Express',
+    lang: lib.en,
+    url: lib.url
+   });
 });
 
 module.exports = router;
