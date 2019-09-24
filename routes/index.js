74556cd152bda79acd7aa1241e448edef8deea41
kiran-rao248 <kiran.rao@doctoroncall.com.my>
2019-09-24 17:35:00 +0800

    changes

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,25 @@ router.get('/', function(req, res, next) {
    });
 });
 
+//get homepage in malay
+router.get('/bm', function(req, res, next) {  
+  res.render('index', { 
+    title: 'Express',
+    lang: lib.bm,  
+    url: lib.url
+   });
+});
+
+// USE THIS AFTER HAVE BUTTON BM/EN
+// router.get('/', function(req, res, next) {
+//   var lang = "en"  
+//   if (req.query.lang == "bm") {
+//     lang = "bm"}
+//      res.render('index', { 
+//     title: 'Express',
+//     lang: lib[lang],
+//     url: lib.url
+//    });
+// });
+
 module.exports = router;
