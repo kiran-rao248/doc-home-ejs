e98db9ffb195049665fda8fcb6190a46fd7156c9
kiran-rao248 <kiran.rao@doctoroncall.com.my>
2019-09-24 18:48:32 +0800

    ask doctor bm done

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,19 @@
 var express = require('express');
 var router = express.Router();
-const lib = require('../config/library'); 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {  
-  res.render('index', { 
-    title: 'Express',
-    lang: lib.en,
-    url: lib.url
-   });
-});
-
-//get homepage in malay
-router.get('/bm', function(req, res, next) {  
-  res.render('index', { 
-    title: 'Express',
-    lang: lib.bm,  
-    url: lib.url
-   });
-});
+const lib = require('../config/library');  
 
 // USE THIS AFTER HAVE BUTTON BM/EN
-// router.get('/', function(req, res, next) {
-//   var lang = "en"  
-//   if (req.query.lang == "bm") {
-//     lang = "bm"}
-//      res.render('index', { 
-//     title: 'Express',
-//     lang: lib[lang],
-//     url: lib.url
-//    });
-// });
+router.get('/', function(req, res, next) {
+  var lang = "en"  
+  if (req.query.lang == "bm") {
+    lang = "bm"
+  }
+     res.render('index', { 
+    title: 'Express',
+    language: lang,
+    lang: lib[lang],
+    url: lib.url
+   });
+});
 
 module.exports = router;
